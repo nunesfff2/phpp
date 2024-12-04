1d0d0ff8cabb93b99a01cf9cd134f57b242a7552
nunesfff2 <fernandonunesdeoliveira880@gmail.com>
2024-12-04 14:54:27 -0400

    Add files via upload

---
+++ fer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testando XAMMP</title>
+</head>
+<body>
+    <?php 
+    echo"Olá Mundo!"
+    ?>
+    
+</body>
+</html>
